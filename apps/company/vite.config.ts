8a5331105ac8c10110b4d5d48ee3d9b2763a8591
1408bg <bgyang1220@gmail.com>
2025-08-20 17:59:57 +0900

    ✨ (CO) :: company 앱 생성

---
+++ apps/company/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig /* , loadEnv */ } from "vite";
+import react from "@vitejs/plugin-react";
+import { version } from "./package.json";
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
+
+// const env = loadEnv("production", import.meta.dirname);
+
+// const sentryPlugin = sentryVitePlugin({
+//   org: "team-return",
+//   project: "jobis-company",
+//   release: {
+//     name: `jobis-company@${version}`,
+//     inject: true,
+//     create: true,
+//     finalize: true,
+//     deploy: {
+//       env: "production"
+//     }
+//   },
+//   authToken: env.VITE_SENTRY_AUTH_TOKEN,
+//   telemetry: false,
+//   sourcemaps: {
+//     assets: ["./dist/assets/**"],
+//     ignore: ["node_modules"]
+//   }
+// });
+
+export default defineConfig({
+  build: {
+    sourcemap: true
+  },
+  define: {
+    "import.meta.env.VITE_APP_DIST": JSON.stringify(new Date().toISOString()),
+    "__APP_VERSION__": JSON.stringify(version)
+  },
+  plugins: [react() /*, sentryPlugin */]
+});
