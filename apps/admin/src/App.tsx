74a800e3642c6b89bb96cba9fd3dfa141406b25d
seungW0o <ssounsowns@gmail.com>
2025-12-10 14:45:31 +0900

    ♻️ (AD) :: Router -> router

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from "react-router-dom";
-import { Router } from "./router";
+import { router } from "./router";
 
 export function App() {
-  return <RouterProvider router={Router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
