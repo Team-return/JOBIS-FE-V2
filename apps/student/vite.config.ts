7916db783e3d60cd3813113248ab9ef9a9ddcbbc
1408bg <bgyang1220@gmail.com>
2025-12-19 18:08:27 +0900

    🧱 (ST) :: sentry 적용 + mode에 따른 환경변수 주입

--- apps/student/vite.config.ts
+++ apps/student/vite.config.ts
@@ -1,38 +1,59 @@
-import { defineConfig /* , loadEnv */ } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { version } from "./package.json";
+import { version, name as projectName } from "./package.json";
 import tsconfigPaths from "vite-tsconfig-paths";
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import path from "path";
+import process from "process";
+import { access, constants } from "fs/promises";
 
-// const env = loadEnv("production", import.meta.dirname);
+type Mode = "development" | "staging" | "production";
 
-// const sentryPlugin = sentryVitePlugin({
-//   org: "team-return",
-//   project: "jobis-student",
-//   release: {
-//     name: `jobis-student@${version}`,
-//     inject: true,
-//     create: true,
-//     finalize: true,
-//     deploy: {
-//       env: "production"
-//     }
-//   },
-//   authToken: env.VITE_SENTRY_AUTH_TOKEN,
-//   telemetry: false,
-//   sourcemaps: {
-//     assets: ["./dist/assets/**"],
-//     ignore: ["node_modules"]
-//   }
-// });
+const envFolderPath = path.join(import.meta.dirname, "../../");
+
+const isLocal = async () => {
+  try {
+    await access(path.join(envFolderPath, ".env"), constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const mode: Mode = (await isLocal())
+  ? "development"
+  : ((process.env.MODE ?? "development") as Mode);
+const env =
+  mode === "development" ? loadEnv(mode, envFolderPath, "") : process.env;
+const sentryProjectName = projectName.split("/").pop();
+
+const sentryPlugin = sentryVitePlugin({
+  org: "team-return",
+  project: sentryProjectName,
+  release: {
+    name: `jobis-${sentryProjectName}@${version}`,
+    inject: true,
+    create: true,
+    finalize: true,
+    deploy: {
+      env: mode
+    }
+  },
+  authToken: env.VITE_SENTRY_AUTH_TOKEN,
+  telemetry: false,
+  sourcemaps: {
+    assets: ["./dist/assets/**"],
+    ignore: ["node_modules"]
+  }
+});
 
 export default defineConfig({
   build: {
     sourcemap: true
   },
   define: {
-    "import.meta.env.VITE_APP_DIST": JSON.stringify(new Date().toISOString()),
-    "__APP_VERSION__": JSON.stringify(version)
+    "import.meta.env.MODE": JSON.stringify(env.MODE),
+    "import.meta.env.BASE_URL": JSON.stringify(env.BASE_URL)
   },
-  plugins: [react() /*, sentryPlugin */, tsconfigPaths()]
+  plugins: [react(), tsconfigPaths(), sentryPlugin]
 });
