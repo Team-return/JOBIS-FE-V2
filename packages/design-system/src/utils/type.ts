650d77e949d8965e13bc88a2aa180657af3fba48
seungW0o <ssounsowns@gmail.com>
2026-01-04 00:21:29 +0900

    ✨ (DS) :: vw, vh 추가

--- packages/design-system/src/utils/type.ts
+++ packages/design-system/src/utils/type.ts
@@ -1,9 +1,13 @@
 import type { ReactNode } from "react";
 
 export type ParentProps<T extends object> = T & { children?: ReactNode };
-export type TextProps<T extends object> = T & { children?: string };
+export type TextProps<T extends object> = T & { children?: string | ReactNode };
 export type PixelValue = number | `${number}px`;
-export type DimensionValue = PixelValue | `${number}%`;
+export type DimensionValue =
+  | PixelValue
+  | `${number}%`
+  | `${number}vw`
+  | `${number}vh`;
 export type BorderValue =
   | `${PixelValue} ${"solid" | "dashed" | "dotted"} ${string}`
   | "none";
