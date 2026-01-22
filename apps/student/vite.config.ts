7fbd7b88815c314f6ca7997b209ada21b8d4917f
1408bg <bgyang1220@gmail.com>
2026-01-22 21:59:07 +0900

    🧱 (ST) :: config 적용

--- apps/student/vite.config.ts
+++ apps/student/vite.config.ts
@@ -1,65 +1,9 @@
-import { type PluginOption, defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import { version, name as projectName } from "./package.json";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import path from "path";
-import process from "process";
-import { access, constants } from "fs/promises";
+import { createViteConfig } from "../../vite.config.common";
+import { name, version } from "./package.json";
 
-type Mode = "development" | "staging" | "production";
-
-const envFolderPath = path.join(import.meta.dirname, "../../");
-
-const isLocal = async () => {
-  try {
-    await access(path.join(envFolderPath, ".env"), constants.F_OK);
-    return true;
-  } catch {
-    return false;
+export default createViteConfig({
+  app: {
+    name,
+    version
   }
-};
-
-const mode: Mode = (await isLocal())
-  ? "development"
-  : ((process.env.MODE ?? "development") as Mode);
-const env =
-  mode === "development" ? loadEnv(mode, envFolderPath, "") : process.env;
-const sentryProjectName = projectName.split("/").pop()!;
-
-const sentryPlugin = sentryVitePlugin({
-  org: "team-return",
-  project: sentryProjectName,
-  release: {
-    name: `jobis-${sentryProjectName}@${version}`,
-    inject: true,
-    create: true,
-    finalize: true,
-    deploy: {
-      env: mode
-    }
-  },
-  authToken: env.SENTRY_AUTH_TOKEN,
-  telemetry: false,
-  sourcemaps: {
-    assets: ["./dist/assets/**"],
-    ignore: ["node_modules"]
-  }
-});
-
-export default defineConfig({
-  build: {
-    sourcemap: true
-  },
-  define: {
-    "import.meta.env.MODE": JSON.stringify(mode),
-    "import.meta.env.BASE_URL": JSON.stringify(env.BASE_URL),
-    "import.meta.env.SENTRY_DSN": JSON.stringify(
-      env[`SENTRY_${sentryProjectName.toUpperCase()}_DSN`]
-    ),
-    "import.meta.env.VERSION": JSON.stringify(version),
-    "import.meta.env.APP_DIST": JSON.stringify(sentryProjectName)
-  },
-  plugins: [react(), tsconfigPaths(), sentryPlugin] as PluginOption[],
-  publicDir: "../../public"
 });
