0eadfed9d1e7f86f4f16ba4a5232b33f28ee2610
seungW0o <ssounsowns@gmail.com>
2026-02-09 16:56:00 +0900

    ✨ (DS) :: Speaker, ArrowLeft icon add

---
+++ packages/design-system/src/icons/ArrowLeft.tsx
@@ -0,0 +1,18 @@
+import type { SVGProps } from "react";
+const SvgArrowLeft = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M8.75 16.25L2.5 10M2.5 10L8.75 3.75M2.5 10H17.5"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+export default SvgArrowLeft;
