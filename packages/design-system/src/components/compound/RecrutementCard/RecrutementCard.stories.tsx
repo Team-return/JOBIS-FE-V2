dedbfbb999b1e29d413adf62bdc33d55b5aa31cf
abc100 <abccba111307@gmail.com>
2026-02-11 22:31:40 +0900

    ✨(ST) :: RecrutementCard 스토리북 type 추가

--- packages/design-system/src/components/compound/RecrutementCard/RecrutementCard.stories.tsx
+++ packages/design-system/src/components/compound/RecrutementCard/RecrutementCard.stories.tsx
@@ -20,9 +20,9 @@ const meta: Meta<typeof RecrutementCard> = {
       control: "text",
       description: "회사 프로필 이미지 URL"
     },
-    trainPay: {
-      control: "number",
-      description: "실습 수당 (만원)"
+    isRecruit: {
+      control: "boolean",
+      description: "모집 여부"
     },
     militarySupport: {
       control: "boolean",
@@ -41,7 +41,7 @@ const meta: Meta<typeof RecrutementCard> = {
     hiringJobs: "프론트엔드 개발자",
     companyName: "자비스",
     companyProfileUrl: "https://placehold.co/222x144",
-    trainPay: 200,
+    isRecruit: true,
     militarySupport: true,
     bookmarked: false
   }
--- packages/design-system/src/components/compound/RecrutementCard/RecrutementCard.test.tsx
+++ packages/design-system/src/components/compound/RecrutementCard/RecrutementCard.test.tsx
@@ -10,6 +10,7 @@ describe("RecrutementCard", () => {
     companyProfileUrl: "https://placehold.co/222x144",
     trainPay: 200,
     militarySupport: true,
+    isRecruit: true,
     bookmarked: false
   };
 
