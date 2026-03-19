3c3988528665ee58b395924b1ed1cf97c12cef9f
yunho <jyh19310302@gmail.com>
2026-03-19 22:36:18 +0900

    ♻️ (ST) :: DetailTable 스토리북 수정

--- packages/design-system/src/components/compound/DetailTable/DetailTable.stories.tsx
+++ packages/design-system/src/components/compound/DetailTable/DetailTable.stories.tsx
@@ -21,16 +21,20 @@ export const Default: Story = {
   args: {
     items: [
       { label: "대표", value: "김하온", itemType: "text" },
-      { label: "서비스 이름", value: "하이티비", itemType: "text" },
       { label: "회사 소개", value: "하이", itemType: "text" },
       {
-        label: "회사 주소(본사)",
-        value: "(60202) 서울특별시 강남구 테헤란로 142 12층 (역삼동, 캐",
+        label: "회사 우편번호",
+        value: "60202",
         itemType: "text"
       },
+      {
+        label: "회사 주소",
+        value: "서울특별시 강남구 테헤란로 142 12층 (역삼동, 캐)",
+        itemType: "text"
+      },
+      { label: "이메일", value: "mittoao205@gmail.com", itemType: "text" },
       { label: "설립일", value: "2006-10-21", itemType: "text" },
-      { label: "직원수", value: "10억명", itemType: "text" },
-      { label: "연매출", value: "10원", itemType: "text" },
+      { label: "연매출", value: "10억", itemType: "text" },
       { label: "사업분야", value: "IT", itemType: "text" },
       {
         label: "첨부파일",
