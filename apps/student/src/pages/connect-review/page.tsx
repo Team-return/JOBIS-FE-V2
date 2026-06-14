98d50c2ef33f253972ebfd287f21c4374a9923df
yunho09 <jyh19310302@gmail.com>
2026-06-14 21:53:22 +0900

    ✨(ST) :: connect-review 페이지 추가

--- apps/student/src/pages/connect-review/page.tsx
+++ apps/student/src/pages/connect-review/page.tsx
@@ -1,5 +1,30 @@
-const ConnectReviewPage = () => {
-  return <div>ConnectReviewPage</div>;
-};
+import { Box, Container, Text, Flex, Button } from "@jobis/design-system";
 
-export default ConnectReviewPage;
+export const ConnectReviewPage = () => {
+  return (
+    <Container $maxWidth={960}>
+      <Box height={123} />
+      <Flex $direction="column" $gap={50}>
+        <Flex $direction="column" $align="center" $gap={16}>
+          <Text $size="h4" $weight="bold">
+            수고하셨습니다!
+          </Text>
+          <Flex $direction="column" $align="center" $gap={8}>
+            <Text $size="h6" $weight="medium" $color="#7F7F7F">
+              토스(회사이름)에서 본 면접 후기를 작성해주세요!
+            </Text>
+            <Text $size="h6" $weight="medium" $color="#7F7F7F">
+              더 많은 학생들이 참고해서 성장 할 수 있습니다!
+            </Text>
+          </Flex>
+        </Flex>
+        <Box width={960}>
+          <Button $variant="outline" $size="lg" $padding={[20, 779, 20, 32]}>
+            면접 후기 작성하기
+          </Button>
+        </Box>
+      </Flex>
+      <Box height={513} />
+    </Container>
+  );
+};
