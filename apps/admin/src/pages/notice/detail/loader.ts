a7835c178aecec7ae6d0475ab3b8ce9e12005c9f
yunho09 <jyh19310302@gmail.com>
2026-09-06 22:25:56 +0900

    ✨ (AD) :: 공지사항 상세 페이지 퍼블리싱

---
+++ apps/admin/src/pages/notice/detail/loader.ts
@@ -0,0 +1,18 @@
+import type { LoaderFunctionArgs } from "react-router-dom";
+import { redirect } from "react-router-dom";
+import { useNoticeDetail } from "@jobis/api";
+import { LoaderData } from "apps/admin/src/utils";
+
+export async function noticeDetailLoader({
+  params
+}: LoaderFunctionArgs): Promise<LoaderData<number>> {
+  const id = Number(params.noticeId);
+
+  if (!Number.isInteger(id) || id <= 0) {
+    throw redirect("/notice");
+  }
+
+  await useNoticeDetail.prefetch(id);
+
+  return { params: id };
+}
