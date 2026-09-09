fe93691a74ceb25d347e2aea64f1692832016995
yunho09 <jyh19310302@gmail.com>
2026-09-09 22:37:14 +0900

    🐛 (ST) :: 레이아웃 main 전체 너비 적용

--- apps/student/src/router.tsx
+++ apps/student/src/router.tsx
@@ -34,7 +34,7 @@ export const router: ReturnType<typeof createBrowserRouter> =
       element: (
         <div style={{ display: "flex", flexDirection: "column" }}>
           <Header type="student" userName="홍길동" />
-          <main style={{ flex: 1 }}>
+          <main style={{ flex: 1, width: "100%" }}>
             <Outlet />
           </main>
           <Footer />
