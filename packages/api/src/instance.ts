47f0aa8593e92dd605e27fe97c7e311ae74efe37
yunho09 <jyh19310302@gmail.com>
2026-09-20 11:51:10 +0900

    🐛 (AP) :: 타임아웃 콜백 호출 누락 수정

--- packages/api/src/instance.ts
+++ packages/api/src/instance.ts
@@ -80,6 +80,7 @@ instance.interceptors.response.use(
   response => response,
   async (error: AxiosError) => {
     if (error.code === "ECONNABORTED") {
+      config.onTimeout?.(error);
       return Promise.reject(
         new Error("서버 응답이 지연되고 있습니다. 잠시 후 다시 시도해주세요.")
       );
